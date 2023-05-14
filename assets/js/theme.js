01f38b9b9627cf9c2efa9493ac1bb6b52c15c21b
Aniket Raj Singh <mohitrajingh223@gmail.com>
2023-05-15 03:40:05 +0530

    Features Page Update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,32 +1,41 @@
 // Custom theme code
 
-if (document.getElementsByClassName('clean-gallery').length > 0) {
-   baguetteBox.run('.clean-gallery', { animation: 'slideIn' });
+if (document.getElementsByClassName("clean-gallery").length > 0) {
+  baguetteBox.run(".clean-gallery", { animation: "slideIn" });
 }
 
-if (document.getElementsByClassName('clean-product').length > 0) {
-    window.onload = function() {
-        vanillaZoom.init('#product-preview');
-    };
+if (document.getElementsByClassName("clean-product").length > 0) {
+  window.onload = function () {
+    vanillaZoom.init("#product-preview");
+  };
 }
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 10) {
     document.getElementById("navbar").style.padding = "1.2rem 1rem";
     document.getElementById("navbar").style.color = "white !important";
-    document.getElementById("navbar").classList.add('bg-nav','navbar-dark');
-    document.getElementById("navbar").classList.remove('bg-transparent' , 'navbar-light');
-    document.getElementById("navbar").style.boxShadow = "0px 0px 15px rgba(255, 255, 255, .1)";
+    document.getElementById("navbar").classList.add("bg-nav", "navbar-dark");
+    document
+      .getElementById("navbar")
+      .classList.remove("bg-transparent", "navbar-light");
+    document.getElementById("navbar").style.boxShadow =
+      "0px 0px 15px rgba(255, 255, 255, .1)";
     document.getElementById("navbar").style.fontFamily = null;
-    
   } else {
-      document.getElementById("navbar").style.fontFamily = "Chalkduster";
-      document.getElementById("navbar").style.color = "black";
+    document.getElementById("navbar").style.fontFamily = "Chalkduster";
+    document.getElementById("navbar").style.color = "black";
     document.getElementById("navbar").style.removeProperty("box-shadow");
-    document.getElementById("navbar").classList.add('bg-transparent', 'navbar-light');
-    document.getElementById("navbar").classList.remove('bg-nav','navbar-dark');
-    
+    document
+      .getElementById("navbar")
+      .classList.add("bg-transparent", "navbar-light");
+    document.getElementById("navbar").classList.remove("bg-nav", "navbar-dark");
   }
 }
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load("particles-js", "assets/js/particles.json", function () {
+  console.log("callback - particles.js config loaded");
+});
