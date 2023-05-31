ebc517ba024f52e380c8f6fa024646916ed39939
Aniket Raj Singh <mohitrajingh223@gmail.com>
2023-05-31 19:08:49 +0530

    SMTP Update

--- Frustrated_Engineer/settings.py
+++ Frustrated_Engineer/settings.py
@@ -39,8 +39,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ietcommunity2@gmail.com'
-EMAIL_HOST_PASSWORD = 'xlovbezkmhtdtbct'
+EMAIL_HOST_USER = 'communityfrustratedengineer@gmail.com'
+EMAIL_HOST_PASSWORD = 'jxndesyaoqpdcpni'
 
 
 # Application definition
