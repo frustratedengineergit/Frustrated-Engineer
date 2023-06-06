24f1503bfba65dd82d8e3d8a22acbf8f94394780
Aniket Raj Singh <mohitrajingh223@gmail.com>
2023-06-06 20:05:01 +0530

    update

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from .models import BlogPost
+from .models import BlogPost, Comment
 from django import forms
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -9,6 +9,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.core.paginator import Paginator
 from markdownx.widgets import MarkdownxWidget
+from .models import Category, Tag
 
 
 @login_required
@@ -23,19 +24,14 @@ def blog_posts(request):
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'categories', 'tags']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': MarkdownxWidget(attrs={'class': 'form-control'}),
+            'categories': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
+            'tags': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
         }
-        categories = forms.ModelMultipleChoiceField(
-        queryset=Category.objects.all(),
-        widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-    )
-    tags = forms.ModelMultipleChoiceField(
-        queryset=Tag.objects.all(),
-        widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-    )
+
 
 class BlogPostCreateView(LoginRequiredMixin, CreateView):
     model = BlogPost
@@ -47,14 +43,15 @@ class BlogPostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class BlogPostDetailView(DetailView):
+
+class BlogPostDetailView(LoginRequiredMixin, DetailView):
     model = BlogPost
     template_name = 'blog_templates/blog_post_detail.html'
     context_object_name = 'post'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = self.object.comments.all()
+        context['comments'] = self.object.comment_set.all()
         return context
 
 
@@ -65,30 +62,21 @@ class BlogPostUpdateView(LoginRequiredMixin, UpdateView):
     context_object_name = 'post'
     success_url = reverse_lazy('blog_posts')
 
+
 class BlogPostDeleteView(LoginRequiredMixin, DeleteView):
     model = BlogPost
     template_name = 'blog_templates/delete_blog_post.html'
     context_object_name = 'post'
     success_url = reverse_lazy('blog_posts')
 
-# Authentication views
 
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('blog_posts')
-        else:
-            messages.error(request, 'Invalid username or password.')
-    return render(request, 'login.html')
+# Authentication views
 
 @login_required
 def logout_view(request):
     logout(request)
     return redirect('blog_posts')
+
 
 @login_required
 def comment_create(request, pk):
