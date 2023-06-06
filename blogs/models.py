f305aead3d4c5eca37ffb000c0dcd12bb232c4a0
Aniket Raj Singh <mohitrajingh223@gmail.com>
2023-06-06 20:03:16 +0530

    update

--- blogs/models.py
+++ blogs/models.py
@@ -1,11 +1,39 @@
-from djongo import models
+from django.db import models
+from markdownx.models import MarkdownxField
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    content = MarkdownxField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    categories = models.ManyToManyField(Category)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on "{self.post.title}"'
--- blogs/views.py
+++ blogs/views.py
@@ -7,11 +7,18 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.core.paginator import Paginator
+from markdownx.widgets import MarkdownxWidget
+
 
 @login_required
 def blog_posts(request):
     posts = BlogPost.objects.all()
-    return render(request, 'blog_templates/blog_post.html', {'posts': posts})
+    paginator = Paginator(posts, 10)  # Set the number of posts to display per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog_templates/blog_post.html', {'page_obj': page_obj})
+
 
 class BlogPostForm(forms.ModelForm):
     class Meta:
@@ -19,8 +26,16 @@ class BlogPostForm(forms.ModelForm):
         fields = ['title', 'content']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'content': MarkdownxWidget(attrs={'class': 'form-control'}),
         }
+        categories = forms.ModelMultipleChoiceField(
+        queryset=Category.objects.all(),
+        widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
+    )
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
+    )
 
 class BlogPostCreateView(LoginRequiredMixin, CreateView):
     model = BlogPost
@@ -36,6 +51,12 @@ class BlogPostDetailView(DetailView):
     model = BlogPost
     template_name = 'blog_templates/blog_post_detail.html'
     context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.all()
+        return context
+
 
 class BlogPostUpdateView(LoginRequiredMixin, UpdateView):
     model = BlogPost
@@ -68,3 +89,11 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('blog_posts')
+
+@login_required
+def comment_create(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        Comment.objects.create(post=post, author=request.user, content=content)
+    return redirect('blog_post_detail', pk=post.pk)
